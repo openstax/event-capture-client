8f6db093b05e01a5c8a9590b959bf7e4283550bd
tom <tom.g.woodward@gmail.com>
2020-12-17 11:28:56 -0500

    couple more tests on create event functionality

--- src/lib/events.spec.ts
+++ src/lib/events.spec.ts
@@ -1,4 +1,5 @@
 import test from 'ava';
+import sinon from 'sinon';
 
 import { createEvent } from "./events";
 
@@ -56,4 +57,41 @@ test('createEvent event where providers provide all input', (t) => {
   const payload = initializedEvent();
 
   t.deepEqual(payload, {testString: 'test', testNumber: 4});
+});
+
+test('createEvent initializes providers when event input is provided', (t) => {
+  const testFormatter = (input: {str: string, num: number} | null | undefined): any => input ? ({testString: input.str, testNumber: input.num}) : null;
+
+  const providerInitializer = sinon.fake.returns(() => ({str: 'test'}));
+  const testProvider: () => () => ({str: string}) = providerInitializer
+
+  const testEventFactory = createEvent(testFormatter,
+    testProvider
+  );
+
+  testEventFactory({num: 4});
+
+  t.assert(providerInitializer.called);
+});
+
+test('createEvent does not reinitialize providers when creating payload', (t) => {
+  const testFormatter = (input: {str: string, num: number} | null | undefined): any => input ? ({testString: input.str, testNumber: input.num}) : null;
+
+  const providerInitializer = sinon.fake.returns(() => ({str: 'test'}));
+  const testProvider: () => () => ({str: string}) = providerInitializer
+
+  const testEventFactory = createEvent(testFormatter,
+    testProvider
+  );
+
+  const initializedEvent = testEventFactory({num: 4});
+
+  t.assert(providerInitializer.calledOnce);
+
+  initializedEvent();
+  initializedEvent();
+  initializedEvent();
+  initializedEvent();
+
+  t.assert(providerInitializer.calledOnce);
 });
