b00851c2489e01c9bab347a6250d437084c1e89d
tom <tom.g.woodward@gmail.com>
2020-12-17 13:16:36 -0500

    set keepalive in client

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,3 +1,8 @@
 import { EventsApi } from "../api/apis/EventsApi";
+import { Configuration } from "../api/runtime";
 
-export const client = new EventsApi();
+const fetchApi: WindowOrWorkerGlobalScope['fetch'] = (input, init = {}) => fetch(input, {...init, keepalive: true});
+
+export const client = new EventsApi(new Configuration({
+  fetchApi
+}));
