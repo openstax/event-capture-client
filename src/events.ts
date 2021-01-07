32e52710b34cf9591f3e7721138b4b45faf34b3f
tom <tom.g.woodward@gmail.com>
2021-01-07 15:21:52 -0500

    add nudged event

--- src/events.ts
+++ src/events.ts
@@ -1,6 +1,7 @@
 import { AccessedStudyguideV1ToJSON, AccessedStudyguideV1TypeEnum } from "./api/models/AccessedStudyguideV1";
 import { CreatedHighlightV1ToJSON, CreatedHighlightV1TypeEnum } from "./api/models/CreatedHighlightV1";
 import { StartedSessionV1ToJSON, StartedSessionV1TypeEnum } from "./api/models/StartedSessionV1";
+import { NudgedV1ToJSON, NudgedV1TypeEnum } from "./api/models/NudgedV1";
 import { createEvent } from "./lib/events";
 import { clientClockProvider, createSessionProvider, typeProvider} from "./providers";
 
@@ -22,6 +23,12 @@ export const accessedStudyguide = createEvent(AccessedStudyguideV1ToJSON,
 
 export const createdHighlight = createEvent(CreatedHighlightV1ToJSON,
   typeProvider(CreatedHighlightV1TypeEnum.OrgOpenstaxEcCreatedHighlightV1),
+  clientClockProvider,
+  sessionProvider
+);
+
+export const nudged = createEvent(NudgedV1ToJSON,
+  typeProvider(NudgedV1TypeEnum.OrgOpenstaxEcNudgedV1),
   clientClockProvider,
   sessionProvider
 );
