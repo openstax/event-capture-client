b6483e42c010b701c7da29743e53aeab1c76df85
tom <tom.g.woodward@gmail.com>
2021-01-07 09:58:50 -0500

    add session started event

--- src/events.ts
+++ src/events.ts
@@ -1,11 +1,18 @@
 import { AccessedStudyguideV1ToJSON, AccessedStudyguideV1TypeEnum } from "./api/models/AccessedStudyguideV1";
 import { CreatedHighlightV1ToJSON, CreatedHighlightV1TypeEnum } from "./api/models/CreatedHighlightV1";
+import { StartedSessionV1ToJSON, StartedSessionV1TypeEnum } from "./api/models/StartedSessionV1";
 import { createEvent } from "./lib/events";
 import { clientClockProvider, createSessionProvider, typeProvider} from "./providers";
 
 export { createEvent };
 
 export const sessionProvider = createSessionProvider();
+
+export const sessionStarted = createEvent(StartedSessionV1ToJSON,
+  typeProvider(StartedSessionV1TypeEnum.OrgOpenstaxEcStartedSessionV1),
+  clientClockProvider,
+  sessionProvider
+);
 
 export const accessedStudyguide = createEvent(AccessedStudyguideV1ToJSON,
   typeProvider(AccessedStudyguideV1TypeEnum.OrgOpenstaxEcAccessedStudyguideV1),
