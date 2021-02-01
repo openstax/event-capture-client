3534b22ea34edef444ff422a154e13dc24dd5e7d
Thomas Woodward <tom.g.woodward@gmail.com>
2021-02-01 16:25:33 -0500

    feat: provide referrer and sourceUri fields (#4)

--- src/events.ts
+++ src/events.ts
@@ -3,7 +3,7 @@ import { CreatedHighlightV1ToJSON, CreatedHighlightV1TypeEnum } from "./api/mode
 import { NudgedV1ToJSON, NudgedV1TypeEnum } from "./api/models/NudgedV1";
 import { StartedSessionV1ToJSON, StartedSessionV1TypeEnum } from "./api/models/StartedSessionV1";
 import { createEvent } from "./lib/events";
-import { clientClockProvider, createSessionProvider, typeProvider} from "./providers";
+import { clientClockProvider, createSessionProvider, referrerProvider, sourceUriProvider, typeProvider } from "./providers";
 
 export { createEvent };
 
@@ -12,23 +12,28 @@ export const sessionProvider = createSessionProvider();
 export const sessionStarted = createEvent(StartedSessionV1ToJSON,
   typeProvider(StartedSessionV1TypeEnum.OrgOpenstaxEcStartedSessionV1),
   clientClockProvider,
+  referrerProvider(),
+  sourceUriProvider(),
   sessionProvider
 );
 
 export const accessedStudyguide = createEvent(AccessedStudyguideV1ToJSON,
   typeProvider(AccessedStudyguideV1TypeEnum.OrgOpenstaxEcAccessedStudyguideV1),
   clientClockProvider,
+  sourceUriProvider(),
   sessionProvider
 );
 
 export const createdHighlight = createEvent(CreatedHighlightV1ToJSON,
   typeProvider(CreatedHighlightV1TypeEnum.OrgOpenstaxEcCreatedHighlightV1),
   clientClockProvider,
+  sourceUriProvider(),
   sessionProvider
 );
 
 export const nudged = createEvent(NudgedV1ToJSON,
   typeProvider(NudgedV1TypeEnum.OrgOpenstaxEcNudgedV1),
   clientClockProvider,
+  sourceUriProvider(),
   sessionProvider
 );
