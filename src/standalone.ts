13f94d39f1d20bf76a8f493fbccb5a721e847049
Dante Soares <dante.m.soares@rice.edu>
2025-05-19 18:58:10 -0500

    If token is set in window, use it to configure EC client

--- src/standalone.ts
+++ src/standalone.ts
@@ -1,6 +1,12 @@
 import * as events from './events';
 import { createCaptureContext } from "./lib/capture";
 
-const { capture } = createCaptureContext();
+declare global {
+  var _OX_AUTH_TOKEN: string | undefined;
+}
+
+const { capture, configure } = createCaptureContext({initialized: !window._OX_AUTH_TOKEN});
+
+if (window._OX_AUTH_TOKEN) { configure({ headers: { Authorization: window._OX_AUTH_TOKEN } }) };
 
 export { capture, events };
