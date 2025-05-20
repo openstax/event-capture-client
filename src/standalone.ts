ea4eabc90e21cade3058aa87e1c0c19513123ab1
Dante Soares <dante.m.soares@rice.edu>
2025-05-20 10:12:01 -0500

    Removed unused file in .github folder, fixed lint

--- src/standalone.ts
+++ src/standalone.ts
@@ -2,11 +2,13 @@ import * as events from './events';
 import { createCaptureContext } from "./lib/capture";
 
 declare global {
-  var _OX_AUTH_TOKEN: string | undefined;
+  var _OX_AUTH_TOKEN: string | undefined; // eslint-disable-line no-var
 }
 
 const { capture, configure } = createCaptureContext({initialized: !window._OX_AUTH_TOKEN});
 
-if (window._OX_AUTH_TOKEN) { configure({ headers: { Authorization: window._OX_AUTH_TOKEN } }) };
+if (window._OX_AUTH_TOKEN) {
+  configure({ headers: { Authorization: window._OX_AUTH_TOKEN } });
+}
 
 export { capture, events };
