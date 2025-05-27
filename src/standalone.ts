fd66c796ba5f77d8a4556a874e931c0c504fef3c
Dante Soares <dante.m.soares@rice.edu>
2025-05-27 16:51:50 -0500

    If token is set in window, use it to configure EC client (#18)
    * If token is set in window, use it to configure EC client
    
    * Fix GH actions
    
    * Removed unused file in .github folder, fixed lint
    
    * Comment for the window var
    
    * The header actually needs to start with Bearer

--- src/standalone.ts
+++ src/standalone.ts
@@ -1,6 +1,16 @@
 import * as events from './events';
 import { createCaptureContext } from "./lib/capture";
 
-const { capture } = createCaptureContext();
+declare global {
+  // This needs to be a var for the window type to actually have that property
+  // eslint-disable-next-line no-var
+  var _OX_AUTH_TOKEN: string | undefined;
+}
+
+const { capture, configure } = createCaptureContext({initialized: !window._OX_AUTH_TOKEN});
+
+if (window._OX_AUTH_TOKEN) {
+  configure({ headers: { Authorization: `Bearer ${window._OX_AUTH_TOKEN}` } });
+}
 
 export { capture, events };
